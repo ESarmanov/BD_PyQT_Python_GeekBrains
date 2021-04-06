aaca3a5fc467dd6f7d35b12abcf26b349e598078
ESarmanov <esarmanov@gmail.com>
2021-04-06 11:30:32 +0500

    ДЗ Lesson 4
    ДЗ Lesson 4
    Базы данных и PyQT

---
+++ Lesson_4_Sarmanov_EF/lib/variables.py
@@ -0,0 +1,42 @@
+import logging
+
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+LOGGING_LEVEL = logging.DEBUG
+#LOGGING_LEVEL = logging.info()
+SERVER_DATABASE = 'sqlite:///server_db.db3'
+SERVER_CONFIG = 'server.ini'
+SERVER_TIMEOUT = 1/2
+
+CLIENT_LISTEN = False  # используется для определения, клиент пишет или слушает
+
+# Прококол JIM основные ключи:
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'from'
+DESTINATION = 'to'
+
+# Прочие ключи, используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+EXIT = 'exit'
+LISTEN = 'listen'  # ключ для словаря, отправка от клиента запрос на прослушивание
+GET_CONTACTS = 'get_contacts'
+LIST_INFO = 'data_list'
+REMOVE_CONTACT = 'remove'
+ADD_CONTACT = 'add'
+USERS_REQUEST = 'get_users'
+
+
+# Словари - ответы:
+RESPONSE_200 = {RESPONSE: 200}
+RESPONSE_202 = {RESPONSE: 202, LIST_INFO: None}
+RESPONSE_400 = {RESPONSE: 400, ERROR: None}
